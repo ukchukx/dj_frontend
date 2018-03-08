e3cd1ad3d4705f91cb212a0d6bfb3f72606cf09a
Uk <ukchukundah@gmail.com>
2018-03-08 16:23:00 +0100

    Add journals

--- pages/journals.vue
+++ pages/journals.vue
@@ -1,55 +1,52 @@
 <template>
   <div class="container">
-    <div class="text-center mt-3" v-if="!journals.length">
+    <div class="text-center mt-3 mb-3" v-if="!journals.length">
       <h1 class="empty-text mb-3">
         You do not have any journals
       </h1>
       <a href="/new" class="new-link">Create a journal</a>
     </div>
-    <b-list-group v-else>
-      <b-list-group-item href="#" class="flex-column align-items-start list-group-item-info">
+    <b-list-group v-else class="mb-3 mt-3">
+      <b-list-group-item 
+        v-for="journal in journals"
+        :key="journal.id"
+        href="javascript:;" :class="getListItemClasses(journal)">
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">List group item heading</h5>
-          <small>3 days ago</small>
+          <h5 class="mb-1">{{ journal.index }}</h5>
+          <small>Created {{ journal.dateString }}</small>
         </div>
         <p class="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas
-          sed diam eget risus varius blandit.
+          {{ journal.decision }}
         </p>
-        <small>Donec id elit non mi porta.</small>
-      </b-list-group-item>
-      <b-list-group-item href="#" class="flex-column align-items-start">
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">List group item heading</h5>
-          <small class="text-muted">3 days ago</small>
-        </div>
-        <p class="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas
-          sed diam eget risus varius blandit.
-        </p>
-        <small class="text-muted">Donec id elit non mi porta.</small>
-      </b-list-group-item>
-      <b-list-group-item href="#" disabled class="flex-column align-items-start">
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">Disabled List group item</h5>
-          <small class="text-muted">3 days ago</small>
-        </div>
-        <p class="mb-1">
-          Donec id elit non mi porta gravida at eget metus. Maecenas
-          sed diam eget risus varius blandit.
-        </p>
-        <small class="text-muted">Donec id elit non mi porta.</small>
+        <small v-if="!journal.whatHappened.length">Can be reviewed {{ journal.reviewDateString }}</small>
       </b-list-group-item>
     </b-list-group>
   </div>
 </template>
 <script>
+import moment from 'moment-mini';
 
 export default {
   layout: 'page',
   computed: {
     journals() {
-      return this.$store.getters.getJournals;
+      return this.$store.getters.getJournals
+        .map(journal => Object.assign(
+          journal, 
+          { 
+            canReview: this.canReview(journal),
+            dateString: moment(journal.createdAt).fromNow(),
+            reviewDateString: moment(journal.reviewDate).fromNow()
+          }
+        ));
+    }
+  },
+  methods: {
+    canReview({ reviewDate, whatHappened }) {
+      return reviewDate <= Date.now() && whatHappened.length === 0;
+    },
+    getListItemClasses({ canReview }) {
+      return `flex-column align-items-start${canReview ? ' list-group-item-info' : ''}`;
     }
   }
 };
